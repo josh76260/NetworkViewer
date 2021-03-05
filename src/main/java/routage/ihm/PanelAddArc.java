a02683272375c08c1390714da8b93903d58139fb
Joshua Galien <joshua.lanson.galien@gmail.com>
2021-03-05 11:55:22 +0100

    correction de bugs

--- src/main/java/routage/ihm/PanelAddArc.java
+++ src/main/java/routage/ihm/PanelAddArc.java
@@ -1,4 +1,102 @@
 package routage.ihm;
 
-public class PanelAddArc {
+import routage.metier.Commutateur;
+import routage.metier.Liaison;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class PanelAddArc extends JPanel implements PanelSaisie {
+    private final AffichageReseau ihm;
+    private JComboBox<Commutateur> lDep;
+    private JComboBox<Commutateur> lArr;
+    private JTextField poids;
+
+
+    public PanelAddArc(AffichageReseau ihm) {
+        super(new GridBagLayout());
+        this.ihm = ihm;
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.CENTER;
+        c.gridwidth = 2;
+        c.insets = new Insets(0, 0, 35, 0);
+
+
+        add(new JLabel("Ajout d'une route "), c);
+
+        c = new GridBagConstraints();
+
+        c.gridx = 0;
+        c.gridy = 3;
+        c.insets = new Insets(0, 0, 25, 0);
+
+        add(new JLabel("Départ : "), c);
+
+        lDep = new JComboBox<>(ihm.getReseau().getCommutateurs().toArray(new Commutateur[0]));
+        lDep.setPreferredSize(new Dimension(50, 20));
+        lDep.addActionListener(ie -> modifArrivee());
+        c.gridx = 1;
+        add(lDep, c);
+
+        ArrayList<Commutateur> lTemp = new ArrayList<>(ihm.getReseau().getCommutateurs());
+        c = new GridBagConstraints();
+
+        if (lArr != null) remove(lArr);
+
+        c.gridx = 0;
+        c.gridy = 6;
+        c.insets = new Insets(0, 0, 25, 0);
+
+        lTemp.removeAll(Liaison.getVoisins((Commutateur) lDep.getSelectedItem()));
+        lTemp.remove(lDep.getSelectedItem());
+        add(new JLabel("Arrivée : "), c);
+        lArr = new JComboBox<>(lTemp.toArray(new Commutateur[0]));
+        lArr.setPreferredSize(new Dimension(50, 20));
+        c.gridx = 1;
+
+        add(lArr, c);
+
+        c = new GridBagConstraints();
+
+        c.gridx = 0;
+        c.gridy = 9;
+        add(new JLabel("poids : "), c);
+
+        poids = new JTextField(20);
+        c.gridx = 1;
+
+        add(poids, c);
+
+        c = new GridBagConstraints();
+
+        c.gridy = 12;
+        c.gridx = 1;
+        JButton supprimer = new JButton("Ajouter");
+        supprimer.addActionListener(ae -> ajouterRoute());
+        add(supprimer, c);
+
+        setVisible(true);
+    }
+
+    private void modifArrivee() {
+        lArr.removeAllItems();
+        ArrayList<Commutateur> lTemp = new ArrayList<>(ihm.getReseau().getCommutateurs());
+        lTemp.removeAll(Liaison.getVoisins((Commutateur) lDep.getSelectedItem()));
+        lTemp.remove(lDep.getSelectedItem());
+        for (Commutateur c: lTemp) {
+            lArr.addItem(c);
+        }
+        repaint();
+        revalidate();
+    }
+
+    private void ajouterRoute() {
+        if(!poids.getText().equals("") && poids.getText().matches("^\\p{Digit}+$")) {
+            Commutateur c = (Commutateur) lDep.getSelectedItem();
+            Liaison.creerLiaison(Integer.parseInt(poids.getText()), c, (Commutateur) lArr.getSelectedItem());
+            ihm.majIHM();
+        }
+    }
 }
--- src/main/java/routage/ihm/PanelDelArc.java
+++ src/main/java/routage/ihm/PanelDelArc.java
@@ -1,4 +1,88 @@
 package routage.ihm;
 
-public class PanelDelArc {
+import routage.metier.Commutateur;
+import routage.metier.Liaison;
+
+import javax.swing.*;
+import javax.xml.stream.events.Comment;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class PanelDelArc extends JPanel implements PanelSaisie {
+    private final AffichageReseau ihm;
+    private JComboBox<Commutateur> lDep;
+    private JComboBox<Commutateur> lArr;
+
+    public PanelDelArc(AffichageReseau ihm) {
+        super(new GridBagLayout());
+        this.ihm = ihm;
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.CENTER;
+        c.gridwidth = 2;
+        c.insets = new Insets(0, 0, 35, 0);
+
+
+        add(new JLabel("Suppression d'une route"), c);
+
+        c = new GridBagConstraints();
+
+        c.gridx = 0;
+        c.gridy = 3;
+        c.insets = new Insets(0, 0, 25, 0);
+
+        add(new JLabel("Départ : "), c);
+
+        lDep = new JComboBox<>(ihm.getReseau().getCommutateurs().toArray(new Commutateur[0]));
+        lDep.setPreferredSize(new Dimension(50, 20));
+        lDep.addActionListener(ie -> modifArrivee());
+        c.gridx = 1;
+        add(lDep, c);
+
+        ArrayList<Commutateur> lTemp = new ArrayList<>();
+        for (Commutateur comm : Liaison.getVoisins(((Commutateur) lDep.getSelectedItem()))) {
+            lTemp.add(comm);
+        }
+        c = new GridBagConstraints();
+
+        if (lArr != null) remove(lArr);
+
+        c.gridx = 0;
+        c.gridy = 6;
+        c.insets = new Insets(0, 0, 25, 0);
+
+        add(new JLabel("Arrivée : "), c);
+        lArr = new JComboBox<>(lTemp.toArray(new Commutateur[0]));
+        lArr.setPreferredSize(new Dimension(50, 20));
+        c.gridx = 1;
+
+        add(lArr, c);
+
+        c = new GridBagConstraints();
+
+        c.gridy = 9;
+        c.gridx = 1;
+        JButton supprimer = new JButton("Supprimer");
+        supprimer.addActionListener(ae -> supprimerRoute());
+        add(supprimer, c);
+
+        setVisible(true);
+    }
+
+    private void supprimerRoute() {
+        Commutateur c = ((Commutateur) lDep.getSelectedItem());
+        Liaison.supprLiaison(c, (Commutateur) lArr.getSelectedItem());
+        ihm.majIHM();
+    }
+
+    private void modifArrivee() {
+        lArr.removeAllItems();
+
+        for (Commutateur comm : Liaison.getVoisins(((Commutateur) lDep.getSelectedItem()))) {
+            lArr.addItem(comm);
+        }
+
+        repaint();
+        revalidate();
+    }
 }
